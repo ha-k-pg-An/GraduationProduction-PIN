e2fc94e2a530cf2fd3efb0c4285d992ef2a39a6d
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2022-12-20 03:12:11 +0900

    タイトル画面完成

--- PIN/Assets/Scripts/Title/PlayBotton.cs
+++ PIN/Assets/Scripts/Title/PlayBotton.cs
@@ -1,14 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class Replay : MonoBehaviour
+public class PlayBotton : MonoBehaviour
 {
-    //public Text botton;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +19,7 @@ public class Replay : MonoBehaviour
 
     public void IsBottonClick()
     {
-       // Debug.Log("123");
+        // Debug.Log("123");
         SceneManager.LoadScene("Game");
     }
 }
