f12fae732f5d0273e353b163d321f92fddbe0af3
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2022-12-19 16:11:08 +0900

    タイヤの回転をまともにした

--- PIN/Assets/Scripts/Tire.cs
+++ PIN/Assets/Scripts/Tire.cs
@@ -4,17 +4,91 @@ using UnityEngine;
 
 public class Tire : MonoBehaviour
 {
+    private float speed;        //回転スピード
+
+    enum RotateStep
+    {
+        STOPPING,            
+        SLOWLEFT,           
+        SLOWRIGHT,
+        FASTLEFT,
+        FASTRIGHT,
+    }
+
+    private float timeCounter;
+    private RotateStep rotateStep;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0.0f;
+        timeCounter = 0.0f;
+        rotateStep = RotateStep.STOPPING;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0f, 0.0f, 100.0f*Time.deltaTime);
+        //transform.Rotate(0.0f, 0.0f, 100.0f*Time.deltaTime);
+        switch(rotateStep)
+        {
+            case RotateStep.STOPPING:
+                Stopping();
+                break;
+            case RotateStep.SLOWLEFT:
+                SlowLeft();
+                break;
+            case RotateStep.SLOWRIGHT:
+                SlowRight();
+                break;
+            case RotateStep.FASTLEFT:
+                FastLeft();
+                break;
+            case RotateStep.FASTRIGHT:
+                FastRight();
+                break;
+        }
+
+        transform.Rotate(0.0f, 0.0f, speed*Time.deltaTime);
+        timeCounter += Time.deltaTime;
+        if(timeCounter >= 1.5f)
+        {
+            rotateStep = (RotateStep)Random.Range(0, 5);
+            timeCounter = 0.0f;
+        }
+
     }
+
+    private void Stopping()
+    {
+        speed--;
+        if(speed <= 0.0f)
+        {
+            speed = 0.0f;
+        }
+    }
+
+    private void SlowLeft()
+    {
+        speed = 100.0f;
+    }
+
+    private void SlowRight()
+    {
+        speed = -100.0f;
+    }
+
+    private void FastLeft()
+    {
+        speed = 200.0f;
+    }
+
+    private void FastRight()
+    {
+        speed = -200.0f;
+    }
+
+
 
     
 }
